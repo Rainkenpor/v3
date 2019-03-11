e4821484a9730a6c4c490cbfa38af65308370903
Kevin Milian <kevinmilian.lemus@gmail.com>
2019-03-11 12:46:27 -0600

    Redirecciones en el mensaje de confirmación

--- application/views/email/success-message.php
+++ application/views/email/success-message.php
@@ -5,9 +5,13 @@
 		    <div class="row">
 		    	<div class="col-md-12 text-center text-white">
 						<h1 class="display-4">Mensaje exitoso</h1>
-						<h5>Gracias por inscribirte a la conferencia de Transformación Digital</h5>
-						<br>
-						<a href="#"><button type="button" class="btn btn-primary" name="button">Agregar a calendario</button></a>
+						<h5>Gracias por inscribirte a la conferencia de Transformación Digital<br> Te hemos enviado un correo de confirmación.</h5>
+						<p>Serás redireccionado en unos segundos.</p>
+						<script>
+							setTimeout(function() {
+							  window.location.href = "<?php echo base_url(); ?>";
+							}, 6000);
+						</script>
 		    	</div>
 			</div>
 		</div>
--- application/views/email/unsuccess-message.php
+++ application/views/email/unsuccess-message.php
@@ -5,7 +5,13 @@
 		    <div class="row">
 		    	<div class="col-md-12 text-center text-white">
 						<h1 class="display-4">Mensaje fallido</h1>
-						<h5>Por favor intenta más tarde</h5>
+						<h5>Por favor intenta más tarde.</h5>
+						<p>Serás redireccionado en unos segundos.</p>
+						<script>
+							setTimeout(function() {
+							  window.location.href = "<?php echo base_url(); ?>";
+							}, 5000);
+						</script>
 		    	</div>
 			</div>
 		</div>
